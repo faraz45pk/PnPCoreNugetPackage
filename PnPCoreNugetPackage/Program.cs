7632481defaf04d8e70ce99e4c3d4bbc8d2d6972
faraz45pk <faraz45pk@hotmail.com>
2016-10-22 11:09:39 +0300

    ok

--- PnPCoreNugetPackage/Program.cs
+++ PnPCoreNugetPackage/Program.cs
@@ -8,27 +8,10 @@ namespace PnPCoreNugetPackage
 {
     class Program
     {
-
-        static void web1() { }
-        static void web2() { }
-        static void web3() { }
-        static void web4() { }
-
-
-        
         static void Main(string[] args)
         {
-
             Console.Write("Helo");
         }
-
-
-        static void visual1() { }
-        static void visual2() { }
-        static void visual3() { }
-
-
-
 
     }
 }
